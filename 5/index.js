4b3374a10ec0fb145af30a3302f3fdc93e068989
Phat <phatben234@gmail.com>
2024-04-24 14:55:25 +0700

    cmt ex5 to understand

--- 5/index.js
+++ 5/index.js
@@ -1,29 +1,29 @@
-let items = [];
+let items = []; // khai báo 1 mảng rỗng 
 
-const itemsDiv = document.getElementById("items")
-const input = document.getElementById("itemInput")
-const storageKey = "items"
+const itemsDiv = document.getElementById("items") // lấy bên HTML id là items gắn vào itemsDiv
+const input = document.getElementById("itemInput") // lấy bên HTML id là itemInput gắn vào input
+const storageKey = "items" //khai báo storageKey và gán giá trị là "items" sử dụng để lưu trữ và truy xuất dữ liệu từ localStorage.
 
 function renderItems(){
-    itemsDiv.innerHTML = null;
+    itemsDiv.innerHTML = null; // gắn rỗng đảm bảo rằng mỗi lần hàm này được gọi, nội dung bên trong phần tử itemsDiv sẽ được xóa đi
 
-    for(const [idx, item] of Object.entries(items)){
-        const container = document.createElement("div")
+    for(const [idx, item] of Object.entries(items)){ // Duyệt qua từng phần tử trong mảng items bằng cách sử dụng phương thức Object.entries() idx: vị trí, item:giá trị
+        const container = document.createElement("div") // Tạo một phần tử <div> mới và gán vào biến container
         container.style.marginBottom = "10px"
         
-        const text = document.createElement("p")
+        const text = document.createElement("p") //Tạo phần tử văn bản mới và gán vào biến text.
         text.style.display = "inline"
         text.style.marginRight = "10px"
         text.textContent = item;
 
-        const button = document.createElement("button")
-        button.textContent = "Delete"
-        button.onclick = () => removeItems(idx)
+        const button = document.createElement("button") // Tạo phần tử nút (<button>) mới và gán vào biến button.
+        button.textContent = "Delete" //Gán nội dung của nút là "Delete".
+        button.onclick = () => removeItems(idx) //Gán sự kiện onclick cho nút, khi nút được nhấn, hàm removeItems(idx) sẽ được gọi để xóa mục tại chỉ số idx.
 
-        container.appendChild(text)
-        container.appendChild(button)
+        container.appendChild(text) //Thêm phần tử văn bản (text) vào phần tử container.
+        container.appendChild(button) //Thêm nút xóa (button) vào phần tử container.
 
-        itemsDiv.appendChild(container)
+        itemsDiv.appendChild(container) //Thêm phần tử container (chứa văn bản và nút xóa) vào phần tử có id là itemsDiv, để hiển thị trên trang web.
     }
 }
 
